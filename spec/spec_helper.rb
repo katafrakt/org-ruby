a5bef33ca4d705bdf439d6d7f6e27b5f146e8f83
Paweł Świątkowski <katafrakt@vivaldi.net>
2025-01-31 23:31:11 +0100

    Replace output files with snapshots
    This will make changes much easier when we can just regenerate snapshots
    and diff them, instead of having to update the expected files manually.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,13 @@
 require "org-ruby"
+require "rspec/snapshot"
+require_relative "support/raw_serializer"
 
 RememberFile = File.join(File.dirname(__FILE__), %w[data remember.org])
 FreeformFile = File.join(File.dirname(__FILE__), %w[data freeform.org])
 FreeformExampleFile = File.join(File.dirname(__FILE__), %w[data freeform-example.org])
 
 RSpec.configure do |config|
+  config.snapshot_dir = "spec/snapshots"
   # == Mock Framework
   #
   # RSpec uses it's own mocking framework by default. If you prefer to
